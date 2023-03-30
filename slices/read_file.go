ff5f0bdd44c7004b8f12553e41417b9694e842ba
levchenko.kk <levchenko.kk@dclouds.ru>
2023-03-30 12:56:14 +0700

    slices

---
+++ slices/read_file.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"os"
+	"regexp"
+)
+
+func main() {
+	IncorrectReadFile()
+	CorrectReadFile()
+}
+
+var (
+	letters = regexp.MustCompile("[a-z]+")
+)
+
+func IncorrectReadFile() []byte {
+	//При чтении файла выделяется массив байт равный размеру(~1mb)
+	//регулярное выражение забирает слайс с тем же базовым массивом,
+	//а полезная для нас инфа содержится только в маленьком слайсе,
+	//но мы будем продолжать тащить за собой огромнывй массив
+	bt, _ := os.ReadFile("file.pdf")
+	return letters.Find(bt)
+}
+
+func CorrectReadFile() []byte {
+	bt, _ := os.ReadFile("file.pdf")
+	lettersBt := letters.Find(bt)
+	lettersBtCopy := make([]byte, len(lettersBt))
+	copy(lettersBtCopy, lettersBt)
+
+	return lettersBtCopy
+}
