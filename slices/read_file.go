b01879449075ff1d4def510335ccaeb894ca177b
levchenko.kk <levchenko.kk@dclouds.ru>
2023-03-30 12:59:06 +0700

    slices

--- slices/read_file.go
+++ slices/read_file.go
@@ -18,12 +18,13 @@ func IncorrectReadFile() []byte {
 	//При чтении файла выделяется массив байт равный размеру(~1mb)
 	//регулярное выражение забирает слайс с тем же базовым массивом,
 	//а полезная для нас инфа содержится только в маленьком слайсе,
-	//но мы будем продолжать тащить за собой огромнывй массив
+	//но мы будем продолжать тащить за собой огромнывй массив.
 	bt, _ := os.ReadFile("file.pdf")
 	return letters.Find(bt)
 }
 
 func CorrectReadFile() []byte {
+	//тут gc всё почистит
 	bt, _ := os.ReadFile("file.pdf")
 	lettersBt := letters.Find(bt)
 	lettersBtCopy := make([]byte, len(lettersBt))
